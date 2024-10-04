c71a3085809035fb32d78623056947f451d8bb01
dornor <151113683+662110205@users.noreply.github.com>
2024-10-04 10:53:41 +0700

    second

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
-import express, { Request, Response } from "express";
+import express,{ Request,Response } from "express";
 import { Utils } from "./Utils";
 
 const app = express()
-const port: number = 3000
+const port:number = 3000
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/',(req:Request, res:Response)=>{
     res.send("hello dornor")
 })
 
-app.get('/addUser', async (req: Request, res: Response) => {
-    const data: any = {
+
+app.get('/addUser',async (req:Request, res:Response)=>{
+    
+    const data:any = {
         "name": "dornor",
-        "username": "misjaeduean"
+        "username": "dornorja"
     }
 
-    const result = await Utils.addUser(data)
+    const result = await Utils.addUser(data) //3 party jsonplaceholder
+    console.log(result)
     res.send(result)
 })
 
-app.listen(port, () => {
-    console.log('server is ' + port);
+app.listen(port, ()=>{
+    console.log('server is '+port);
 })
+
+//web brow localhost:3000?userid=1 <---res          (ts)myserver (addUser) <--res-- (3 party) jsonplace... users
